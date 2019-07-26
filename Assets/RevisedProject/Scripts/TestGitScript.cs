2d987351ba5a9d03450c3c5a0395a927aaa8a6f4
HEPHAESTUS\Josh <josh.folsom@yahoo.com>
2019-07-25 18:53:00 -0700

    Update TestGitScript.cs

--- Assets/RevisedProject/Scripts/TestGitScript.cs
+++ Assets/RevisedProject/Scripts/TestGitScript.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class TestGitScript : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        TestFunc();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void TestFunc()
+    {
+        print("Testing out Git Branches");
+        print("Testing out Git Branches");
+        print("Testing out in a different way");
     }
 }
